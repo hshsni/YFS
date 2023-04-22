efe0ba2d503775da401988da87bfd8de9fe9656e
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-04-22 10:02:40 +0530

    updated Responsive

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:youth_compass_application/Login%20Files/SignUpEmail.dart';
 import 'package:youth_compass_application/Login%20Files/SignUpPhone.dart';
+import 'package:youth_compass_application/Login%20Files/confirmed.dart';
 import 'Login Files/login_page.dart';
 import 'Login Files/otpPage.dart';
 
